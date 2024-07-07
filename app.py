79e1e998135ae22ed63d626302b5a808654d4f31
Andrés Chapo <11888868+AndresChapo@users.noreply.github.com>
2024-07-07 15:30:00 -0300

    Implemented app fixture

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from handlers.routes import configure_routes
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    return app
 
+app = create_app()
 configure_routes(app)
 
 if __name__ == '__main__':
---
+++ tests/conftest.py
@@ -0,0 +1,113 @@
+import json
+import pytest
+from app import create_app
+
+class Constants:
+    class HttpHeaders:
+        AUTHORIZATION = 'Authorization'
+
+access_token=''
+
+@pytest.fixture(autouse=True)
+def app():
+    app = create_app()
+    app.config["TESTING"] = True
+    yield app
+
+
+@pytest.fixture
+def api_client(client):
+    class ApiClient:
+        def __init__(self):
+            self.client = client
+
+        def _check_headers(self, headers=None, authenticated=False):
+            if not headers:
+                headers = [("Accept", "application/json")]
+            if authenticated:
+                headers.append(
+                    (Constants.HttpHeaders.AUTHORIZATION, "Bearer " + access_token)
+                )
+            return headers
+
+        def post(self, url, data, headers=None, environ_base=None, authenticated=True):
+            headers = self._check_headers(headers=headers, authenticated=authenticated)
+
+            return self.client.post(
+                url,
+                data=json.dumps(data),
+                content_type="application/json",
+                headers=headers,
+                environ_base=environ_base,
+            )
+
+        def post_form(self, url, data, headers, environ_base=None, authenticated=True):
+            headers = self._check_headers(headers=headers, authenticated=authenticated)
+
+            return self.client.post(
+                url,
+                data=data,
+                content_type="application/x-www-form-urlencoded",
+                headers=headers,
+                environ_base=environ_base,
+            )
+
+        def post_form_file(
+            self, url, data, headers, environ_base=None, authenticated=True
+        ):
+            headers = self._check_headers(headers=headers, authenticated=authenticated)
+
+            return self.client.post(
+                url,
+                data=data,
+                content_type="multipart/form-data",
+                headers=headers,
+                environ_base=environ_base,
+            )
+
+        def patch_form(self, url, data, headers, environ_base=None, authenticated=True):
+            headers = self._check_headers(headers=headers, authenticated=authenticated)
+
+            return self.client.patch(
+                url,
+                data=data,
+                content_type="multipart/form-data",
+                headers=headers,
+                environ_base=environ_base,
+            )
+
+        def patch(self, url, data, headers=None):
+            headers = self._check_headers(headers=headers)
+
+            return self.client.patch(url, json=data, headers=headers)
+
+        def get(
+            self,
+            url,
+            headers=None,
+            query_string=None,
+            environ_base=None,
+            authenticated=True,
+        ):
+            headers = self._check_headers(headers=headers, authenticated=authenticated)
+
+            return self.client.get(
+                url,
+                content_type="application/json",
+                headers=headers,
+                query_string=query_string,
+                environ_base=environ_base,
+            )
+
+        def delete(self, url, data, headers=None):
+            headers = self._check_headers(headers=headers)
+
+            return self.client.delete(
+                url,
+                data=json.dumps(data),
+                content_type="application/json",
+                headers=headers,
+            )
+
+    return ApiClient()
+
