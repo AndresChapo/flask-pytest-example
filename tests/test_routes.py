c0e2c233ac7ee245f3307a6ce748e5bcc31aab85
Andrés Chapo <11888868+AndresChapo@users.noreply.github.com>
2024-07-10 22:03:34 -0300

    Document and implement api_client

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,4 @@
-import json
+
 
 class TestRoutes:
     """ Examples using pytest fixtures """
@@ -11,27 +11,8 @@ class TestRoutes:
         assert response.get_data() == b'pong'
         assert response.status_code == 200
 
-    def test_post_route__success(self, app):
-        client = app.test_client()
-        url = '/post/test'
-
-        mock_request_headers = [(
-            'authorization-sha256', '123'
-        )]
-
-        mock_request_data = {
-            'request_id': '123',
-            'payload': {
-                'py': 'pi',
-                'java': 'script'
-            }
-        }
-
-
-        response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
-        assert response.status_code == 200
-
-    def test_post_route__success_2(self, api_client):
+    def test_post_route__success(self, api_client):
+        """Test post/test happy path"""
         url = '/post/test'
 
         mock_request_headers = [(
@@ -50,11 +31,12 @@ class TestRoutes:
         assert response.status_code == 200
 
 
-    def test_post_route__failure__unauthorized(self, app):
-        client = app.test_client()
+    def test_post_route__failure__unauthorized(self, api_client):
+        """Run post/test without auth"""
+
         url = '/post/test'
 
-        mock_request_headers = {}
+        mock_request_headers = [] # As we don't send the auth token it will fail
 
         mock_request_data = {
             'request_id': '123',
@@ -64,19 +46,19 @@ class TestRoutes:
             }
         }
 
-        response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
+        response = api_client.post(url, data=mock_request_data, headers=mock_request_headers)
         assert response.status_code == 401
 
 
-    def test_post_route__failure__bad_request(self, app):
-        client = app.test_client()
+    def test_post_route__failure__bad_request(self, api_client):
+        """Run post/test and provoke a bad request"""
         url = '/post/test'
 
-        mock_request_headers = {
-            'authorization-sha256': '123'
-        }
+        mock_request_headers = [(
+            'authorization-sha256', '123'
+        )]
 
-        mock_request_data = {}
+        mock_request_data = {} # As we don't send the payload it will fail
 
-        response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
+        response = api_client.post(url, data=mock_request_data, headers=mock_request_headers)
         assert response.status_code == 400
