ea7d4235b25cf0a5d12d3086be805f4c776f5ffb
Andrés Chapo <11888868+AndresChapo@users.noreply.github.com>
2024-07-22 22:07:02 -0300

    Implement singleton

---
+++ models/singleton.py
@@ -0,0 +1,16 @@
+
+class Singleton(type):
+    """
+    Singleton provide a global point of access to a one instance and it's responsible to create that instance.
+    This has to be use as a metaclass.
+    It will not allow to create multiple instances of the same class, instead it will return the existent one.
+    Use with caution, the Singleton pattern is for specific use cases, ie: web/db connections, buffer, device access, logs
+    """
+    _instances = {}
+    
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+    
