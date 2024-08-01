31f2d67fe89ed35bfb3394f5c805ea086aa78286
Andrés Chapo <11888868+AndresChapo@users.noreply.github.com>
2024-08-01 17:22:16 -0300

    Update singleton.py

--- models/singleton.py
+++ models/singleton.py
@@ -1,8 +1,8 @@
 
 class Singleton(type):
     """
-    Singleton provide a global point of access to a one instance and it's responsible to create that instance.
-    This has to be use as a metaclass.
+    Singleton provide a global point of access to one instance and it's responsible to create that instance.
+    This has to be use as a metaclass, ie: class Logger(metaclass=Singleton)
     It will not allow to create multiple instances of the same class, instead it will return the existent one.
     Use with caution, the Singleton pattern is for specific use cases, ie: web/db connections, buffer, device access, logs
     """
